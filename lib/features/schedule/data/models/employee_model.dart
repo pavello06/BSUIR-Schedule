25b5fde658e21cfd501e62b206af4997df06ddb3
Pavel Halukha <pavelgaluha@gmail.com>
2025-08-13 23:03:44 +0300

    feat: add Employee model

---
+++ lib/features/schedule/data/models/employee_model.dart
@@ -0,0 +1,57 @@
+class EmployeeModel {
+  EmployeeModel({
+    required this.firstName,
+    required this.lastName,
+    required this.middleName,
+    required this.degree,
+    required this.rank,
+    required this.photoLink,
+    required this.calendarId,
+    required this.academicDepartment,
+    required this.id,
+    required this.urlId,
+    required this.fio,
+  });
+
+  final String firstName;
+  final String lastName;
+  final String? middleName;
+  final String degree;
+  final String? rank;
+  final String photoLink;
+  final String? calendarId;
+  final List<String> academicDepartment;
+  final int id;
+  final String urlId;
+  final String fio;
+
+  factory EmployeeModel.fromJson(Map<String, dynamic> json) {
+    return EmployeeModel(
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      middleName: json['middleName'],
+      degree: json['degree'],
+      rank: json['rank'],
+      photoLink: json['photoLink'],
+      calendarId: json['calendarId'],
+      academicDepartment: List<String>.from(json['academicDepartment']),
+      id: json['id'],
+      urlId: json['urlId'],
+      fio: json['fio'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'firstName': firstName,
+    'lastName': lastName,
+    'middleName': middleName,
+    'degree': degree,
+    'rank': rank,
+    'photoLink': photoLink,
+    'calendarId': calendarId,
+    'academicDepartment': academicDepartment,
+    'id': id,
+    'urlId': urlId,
+    'fio': fio,
+  };
+}
