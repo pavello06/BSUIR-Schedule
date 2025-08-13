bdaa9581980554c05784ee4d0b2a033aca07d073
Pavel Halukha <pavelgaluha@gmail.com>
2025-08-13 23:20:51 +0300

    feat: add ScheduleLastUpdate mapper

---
+++ lib/features/schedule/data/mappers/schedule_last_update_mapper.dart
@@ -0,0 +1,14 @@
+import 'package:intl/intl.dart';
+
+import '../../domain/entities/schedule_last_update.dart';
+import '../models/schedule_last_update_model.dart';
+
+class ScheduleLastUpdateMapper {
+  static ScheduleLastUpdate toEntity({
+    required ScheduleLastUpdateModel scheduleLastUpdate,
+  }) {
+    return ScheduleLastUpdate(
+      dateTime: DateFormat('dd.MM.yyyy').parse(scheduleLastUpdate.lastUpdateDate),
+    );
+  }
+}
