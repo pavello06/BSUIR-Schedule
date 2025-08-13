93a1188c6fcaa71ee5fd31677cee5e085d34311f
Pavel Halukha <pavelgaluha@gmail.com>
2025-08-13 23:23:08 +0300

    feat: add Employee mapper

---
+++ lib/features/schedule/data/mappers/employee_mapper.dart
@@ -0,0 +1,18 @@
+import '../../domain/entities/employee.dart';
+import '../models/employee_model.dart';
+
+class EmployeeMapper {
+  static Employee toEntity({required EmployeeModel employee}) {
+    return Employee(
+      firstName: employee.firstName,
+      lastName: employee.lastName,
+      middleName: employee.middleName,
+      degreeAbbrev: employee.degree,
+      rank: employee.rank,
+      photoLink: employee.photoLink,
+      academicDepartment: employee.academicDepartment,
+      urlId: employee.urlId,
+      email: null,
+    );
+  }
+}
