862a51a3bb964b8807a68753c0021af0043b9b43
Pavel Halukha <pavelgaluha@gmail.com>
2025-08-13 23:04:42 +0300

    feat: add Subject model

---
+++ lib/features/schedule/data/models/subject_employee_model.dart
@@ -0,0 +1,65 @@
+class SubjectEmployeeModel {
+  SubjectEmployeeModel({
+    required this.id,
+    required this.firstName,
+    required this.middleName,
+    required this.lastName,
+    required this.photoLink,
+    required this.degree,
+    required this.degreeAbbrev,
+    required this.rank,
+    required this.email,
+    required this.urlId,
+    required this.calendarId,
+    required this.jobPositions,
+    required this.chief,
+  });
+
+  final int id;
+  final String firstName;
+  final String? middleName;
+  final String lastName;
+  final String photoLink;
+  final String degree;
+  final String degreeAbbrev;
+  final String? rank;
+  final String? email;
+  final String urlId;
+  final String? calendarId;
+  final String? jobPositions;
+  final bool chief;
+
+  factory SubjectEmployeeModel.fromJson(Map<String, dynamic> json) {
+    return SubjectEmployeeModel(
+      id: json['id'],
+      firstName: json['firstName'],
+      middleName: json['middleName'],
+      lastName: json['lastName'],
+      photoLink: json['photoLink'],
+      degree: json['degree'],
+      degreeAbbrev: json['degreeAbbrev'],
+      rank: json['rank'],
+      email: json['email'],
+      urlId: json['urlId'],
+      calendarId: json['calendarId'],
+      jobPositions: json['jobPositions'],
+      chief: json['chief'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'firstName': firstName,
+    'middleName': middleName,
+    'lastName': lastName,
+    'photoLink': photoLink,
+    'degree': degree,
+    'degreeAbbrev': degreeAbbrev,
+    'rank': rank,
+    'email': email,
+    'urlId': urlId,
+    'calendarId': calendarId,
+    'jobPositions': jobPositions,
+    'chief': chief,
+  };
+}
---
+++ lib/features/schedule/data/models/subject_model.dart
@@ -0,0 +1,88 @@
+import 'subject_employee_model.dart';
+import 'subject_group_model.dart';
+
+class SubjectModel {
+  SubjectModel({
+    required this.auditories,
+    required this.endLessonTime,
+    required this.lessonTypeAbbrev,
+    required this.note,
+    required this.numSubgroup,
+    required this.startLessonTime,
+    required this.studentGroups,
+    required this.subject,
+    required this.subjectFullName,
+    required this.weekNumber,
+    required this.employees,
+    required this.dateLesson,
+    required this.startLessonDate,
+    required this.endLessonDate,
+    required this.split,
+    required this.announcement,
+  });
+
+  final List<String>? auditories;
+  final String endLessonTime;
+  final String lessonTypeAbbrev;
+  final String? note;
+  final int numSubgroup;
+  final String startLessonTime;
+  final List<SubjectGroupModel> studentGroups;
+  final String subject;
+  final String subjectFullName;
+  final List<int> weekNumber;
+  final List<SubjectEmployeeModel>? employees;
+  final String? dateLesson;
+  final String? startLessonDate;
+  final String? endLessonDate;
+  final bool split;
+  final bool announcement;
+
+  factory SubjectModel.fromJson(Map<String, dynamic> json) {
+    return SubjectModel(
+      auditories: json['auditories'] == null
+          ? null
+          : List<String>.from(json['auditories']),
+      endLessonTime: json['endLessonTime'],
+      lessonTypeAbbrev: json['lessonTypeAbbrev'],
+      note: json['note'],
+      numSubgroup: json['numSubgroup'],
+      startLessonTime: json['startLessonTime'],
+      studentGroups: List<SubjectGroupModel>.from(
+        json['studentGroups']!.map((x) => SubjectGroupModel.fromJson(x)),
+      ),
+      subject: json['subject'],
+      subjectFullName: json['subjectFullName'],
+      weekNumber: List<int>.from(json['weekNumber']),
+      employees: json['employees'] == null
+          ? null
+          : List<SubjectEmployeeModel>.from(
+              json['employees']!.map((x) => SubjectEmployeeModel.fromJson(x)),
+            ),
+      dateLesson: json['dateLesson'],
+      startLessonDate: json['startLessonDate'],
+      endLessonDate: json['endLessonDate'],
+      split: json['split'],
+      announcement: json['announcement'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'auditories': auditories,
+    'endLessonTime': endLessonTime,
+    'lessonTypeAbbrev': lessonTypeAbbrev,
+    'note': note,
+    'numSubgroup': numSubgroup,
+    'startLessonTime': startLessonTime,
+    'studentGroups': studentGroups.map((x) => x.toJson()).toList(),
+    'subject': subject,
+    'subjectFullName': subjectFullName,
+    'weekNumber': weekNumber,
+    'employees': employees?.map((x) => x.toJson()).toList(),
+    'dateLesson': dateLesson,
+    'startLessonDate': startLessonDate,
+    'endLessonDate': endLessonDate,
+    'split': split,
+    'announcement': announcement,
+  };
+}
