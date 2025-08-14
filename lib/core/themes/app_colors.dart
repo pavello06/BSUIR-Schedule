20ea3f2213e5ac54566d6582697aac5c153b1c64
Pavel Halukha <pavelgaluha@gmail.com>
2025-08-14 23:27:26 +0300

    feat: add themes

---
+++ lib/core/themes/app_colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class AppColors {
+  static const primaryColor = Color(0xff0724ba);
+
+  static const foregroundColor = Color(0xffffffff);
+
+  static const lightBackgroundColor = Color(0xffffffff);
+
+  static const darkBackgroundColor = Color(0xff181818);
+
+  static const lightGrey = Color(0xffe0e0e0);
+  static const grey = Color(0xffb3b3b3);
+  static const darkGrey = Color(0xff333333);
+}
---
+++ lib/core/themes/light_theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+import 'app_theme.dart';
+
+class LightTheme implements AppTheme {
+  const LightTheme({required this.primaryColor});
+
+  final Color primaryColor;
+
+  @override
+  ThemeData get themeData => ThemeData.light().copyWith(
+    primaryColor: primaryColor,
+    scaffoldBackgroundColor: AppColors.lightBackgroundColor,
+    appBarTheme: AppBarTheme(
+      backgroundColor: primaryColor,
+      foregroundColor: AppColors.foregroundColor,
+    ),
+    cardTheme: CardThemeData(color: AppColors.lightGrey),
+  );
+}
