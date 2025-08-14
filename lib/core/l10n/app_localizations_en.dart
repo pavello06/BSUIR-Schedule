e3f537e754120d2b46a58a548a78cbb602b33254
Pavel Halukha <pavelgaluha@gmail.com>
2025-08-14 23:20:36 +0300

    feat: add localization

---
+++ lib/core/l10n/app_localizations_en.dart
@@ -0,0 +1,57 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String courseNumber(Object number) {
+    return '$number course';
+  }
+
+  @override
+  String get addingSchedule => 'Adding a schedule';
+
+  @override
+  String get groups => 'Groups';
+
+  @override
+  String get teachers => 'Teachers';
+
+  @override
+  String get search => 'Search';
+
+  @override
+  String get course => 'course';
+
+  @override
+  String foundedGroups(num count) {
+    String _temp0 = intl.Intl.pluralLogic(
+      count,
+      locale: localeName,
+      other: '$count groups found',
+      one: '$count group found',
+      zero: '$count groups found',
+    );
+    return '$_temp0';
+  }
+
+  @override
+  String foundedTeachers(num count) {
+    String _temp0 = intl.Intl.pluralLogic(
+      count,
+      locale: localeName,
+      other: '$count teachers found',
+      one: '$count teacher found',
+      zero: '$count teachers found',
+    );
+    return '$_temp0';
+  }
+
+  @override
+  String get searchEmpty => 'Couldn\'t load schedule\nPull down to update';
+}
---
+++ lib/core/l10n/app_localizations_ru.dart
@@ -0,0 +1,62 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Russian (`ru`).
+class AppLocalizationsRu extends AppLocalizations {
+  AppLocalizationsRu([String locale = 'ru']) : super(locale);
+
+  @override
+  String courseNumber(Object number) {
+    return '$number курс';
+  }
+
+  @override
+  String get addingSchedule => 'Добавление расписания';
+
+  @override
+  String get groups => 'Группы';
+
+  @override
+  String get teachers => 'Преподаватели';
+
+  @override
+  String get search => 'Поиск';
+
+  @override
+  String get course => 'курс';
+
+  @override
+  String foundedGroups(num count) {
+    String _temp0 = intl.Intl.pluralLogic(
+      count,
+      locale: localeName,
+      other: 'Найдено $count групп',
+      many: 'Найдено $count групп',
+      few: 'Найдено $count группы',
+      one: 'Найдена $count группа',
+      zero: 'Найдено $count групп',
+    );
+    return '$_temp0';
+  }
+
+  @override
+  String foundedTeachers(num count) {
+    String _temp0 = intl.Intl.pluralLogic(
+      count,
+      locale: localeName,
+      other: 'Найдено $count преподавателей',
+      many: 'Найдено $count преподавателей',
+      few: 'Найдено $count преподавателя',
+      one: 'Найден $count преподаватель',
+      zero: 'Найдено $count преподавателей',
+    );
+    return '$_temp0';
+  }
+
+  @override
+  String get searchEmpty =>
+      'Не удалось загрузить расписание\nПотяните вниз, чтобы обновить';
+}
