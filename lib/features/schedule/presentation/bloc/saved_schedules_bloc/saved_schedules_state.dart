fd16f475b0f6d52bd20ed809b416689bcc02386a
Pavel Halukha <pavelgaluha@gmail.com>
2025-08-16 10:30:46 +0300

    feat: add SavedSchedule event

---
+++ lib/features/schedule/presentation/bloc/saved_schedules_bloc/saved_schedules_state.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+
+import '../../../domain/entities/saved_schedule.dart';
+
+abstract class SavedSchedulesState extends Equatable {
+  const SavedSchedulesState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class InitialState extends SavedSchedulesState {
+  const InitialState();
+}
+
+class LoadingState extends SavedSchedulesState {
+  const LoadingState();
+}
+
+class LoadedState extends SavedSchedulesState {
+  const LoadedState({required this.savedScheduleList});
+
+  final List<SavedSchedule> savedScheduleList;
+
+  @override
+  List<Object?> get props => [...super.props, savedScheduleList];
+}
+
+class EmptyState extends SavedSchedulesState {
+  const EmptyState();
+}
