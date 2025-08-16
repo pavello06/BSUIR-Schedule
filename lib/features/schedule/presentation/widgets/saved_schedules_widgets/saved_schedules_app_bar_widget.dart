782762fc31ee2ff336ca6758d6d427692072b742
Pavel Halukha <pavelgaluha@gmail.com>
2025-08-16 10:32:14 +0300

    feat: add SavedSchedule widgets

---
+++ lib/features/schedule/presentation/widgets/saved_schedules_widgets/saved_schedules_app_bar_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/extensions/build_context_extension.dart';
+
+class SavedSchedulesAppBarWidget extends StatelessWidget
+    implements PreferredSizeWidget {
+  const SavedSchedulesAppBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back)),
+      title: Text(
+        context.locale.mySchedules,
+        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+      actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+      centerTitle: true,
+      toolbarHeight: preferredSize.height,
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
