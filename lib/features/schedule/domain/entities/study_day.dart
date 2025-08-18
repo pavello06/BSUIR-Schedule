a7116bf0ac0134fd52846e22bf165882e92c545a
Pavel Halukha <pavelgaluha@gmail.com>
2025-08-18 17:26:38 +0300

    feat: add StudyDay entity

---
+++ lib/features/schedule/domain/entities/study_day.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+
+import 'subject.dart';
+
+class StudyDay extends Equatable {
+  const StudyDay({required this.dateTime, required this.subjects});
+
+  final DateTime dateTime;
+  final List<Subject> subjects;
+
+  @override
+  List<Object?> get props => [dateTime, subjects];
+}
